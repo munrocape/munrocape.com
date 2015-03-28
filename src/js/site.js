050244f9f1f7d812edc7050bb3e252745f713526
Zach Munro-Cape <zach.munrocape@gmail.com>
2015-03-28 15:56:41 -0300

    Added a navbar

--- src/js/site.js
+++ src/js/site.js
@@ -1,29 +1,16 @@
 $(document).ready(function() {
 
   // Variables
-  var $codeSnippets = $('.code-example-body'),
-      $nav = $('.navbar'),
+  var $nav = $('.navbar'),
       $body = $('body'),
       $window = $(window),
-      $popoverLink = $('[data-popover]'),
       navOffsetTop = $nav.offset().top,
-      $document = $(document),
-      entityMap = {
-        "&": "&amp;",
-        "<": "&lt;",
-        ">": "&gt;",
-        '"': '&quot;',
-        "'": '&#39;',
-        "/": '&#x2F;'
-      }
+      $document = $(document)
 
   function init() {
     $window.on('scroll', onScroll)
     $window.on('resize', resize)
-    $popoverLink.on('click', openPopover)
-    $document.on('click', closePopover)
     $('a[href^="#"]').on('click', smoothScroll)
-    buildSnippets();
   }
 
   function smoothScroll(e) {
@@ -40,25 +27,11 @@ $(document).ready(function() {
     });
   }
 
-  function openPopover(e) {
-    e.preventDefault()
-    closePopover();
-    var popover = $($(this).data('popover'));
-    popover.toggleClass('open')
-    e.stopImmediatePropagation();
-  }
-
-  function closePopover(e) {
-    if($('.popover.open').length > 0) {
-      $('.popover').removeClass('open')
-    }
-  }
-
   $("#button").click(function() {
     $('html, body').animate({
         scrollTop: $("#elementtoScrollToID").offset().top
     }, 2000);
-});
+  });
 
   function resize() {
     $body.removeClass('has-docked-nav')
@@ -75,20 +48,5 @@ $(document).ready(function() {
     }
   }
 
-  function escapeHtml(string) {
-    return String(string).replace(/[&<>"'\/]/g, function (s) {
-      return entityMap[s];
-    });
-  }
-
-  function buildSnippets() {
-    $codeSnippets.each(function() {
-      var newContent = escapeHtml($(this).html())
-      $(this).html(newContent)
-    })
-  }
-
-
   init();
-
 });
